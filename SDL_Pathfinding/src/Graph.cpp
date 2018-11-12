75fe33363443f39593563c98f206ecd9c1b77376
Unknown <guillemserralorenz@enti.cat>
2018-11-12 13:28:33 +0100

    class 12-11-18

--- SDL_Pathfinding/src/Graph.cpp
+++ SDL_Pathfinding/src/Graph.cpp
@@ -8,6 +8,9 @@ Graph::Graph(std::vector<std::vector<int>> *terrain)
 	num_cell_x = SRC_WIDTH / CELL_SIZE;
 	num_cell_y = SRC_HEIGHT / CELL_SIZE;
 
+	std::cout << num_cell_x << std::endl;
+	std::cout << num_cell_y << std::endl;
+
 	//initialize nodes:
 	initNodesMap(terrain);
 
@@ -19,7 +22,7 @@ void Graph::initNodesMap(std::vector<std::vector<int>> *terrain)
 {
 	//for (int i = 0; i < terrain->size(); i++)
 	//{
-	//	for (int j = 0; j < terrain[0].size(); j++)//si poso '1' en comptes de 'i', peta
+	//	for (int j = 0; j < (*terrain)[i].size(); j++)//si poso '1' en comptes de 'i', peta
 	//	//for (int j = 0; j < terrain->at(i).size(); j++)
 	//	{
 	//		nodesMap.insert(std::pair<std::pair<int, int>, Node*> (std::make_pair(i,j), new Node(i,j)));
@@ -27,9 +30,9 @@ void Graph::initNodesMap(std::vector<std::vector<int>> *terrain)
 	//	//std::cout << i;//a la segona 'i', peta.
 	//}
 
-	for (int i = 0; i < num_cell_x; i++)
+	for (int i = 0; i < num_cell_x; i++)//cols
 	{
-		for (int j = 0; j < num_cell_y; j++)
+		for (int j = 0; j < num_cell_y; j++)//rows
 		{
 			nodesMap.insert(std::pair<std::pair<int, int>, Node*> (std::make_pair(i,j), new Node(i,j)));
 		}
@@ -39,21 +42,26 @@ void Graph::initNodesMap(std::vector<std::vector<int>> *terrain)
 
 void Graph::initEdgesMap(std::vector<std::vector<int>> *terrain)
 {
+	//for (int i = 0; i < terrain->size(); i++)
+	//{
+	//	for (int j = 0; j < (*terrain)[i].size(); j++)//si poso '1' en comptes de 'i', peta
 	for (int i = 0; i < num_cell_x; i++)
 	{
 		for (int j = 0; j < num_cell_y; j++)
 		{
-			if (&terrain[i][j] != 0)
+			std::cout << i << " " << j << std::endl;
+			if ((*terrain)[j][i] != 0)
 			{
+				
 				//look for all other nodes behind it
-				CheckAndConnectNeighborNodeMap(i, j, -1, -1, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, -1, 0, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, -1, 1, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, 0, -1, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, 0, 1, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, 1, -1, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, 1, 0, terrain);
-				CheckAndConnectNeighborNodeMap(i, j, 1, 1, terrain);
+				CheckAndConnectNeighborNodeMap(j, i, -1, -1, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, -1, 0, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, -1, 1, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, 0, -1, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, 0, 1, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, 1, -1, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, 1, 0, terrain);
+				CheckAndConnectNeighborNodeMap(j,i, 1, 1, terrain);
 			}
 		}
 	}
@@ -62,7 +70,7 @@ void Graph::initEdgesMap(std::vector<std::vector<int>> *terrain)
 
 void Graph::CheckAndConnectNeighborNodeMap(int i, int j, int ni, int nj, std::vector<std::vector<int>> *terrain)
 {
-	if (i + ni >= 0 && i + ni < num_cell_x && j + nj >= 0 && j + nj < num_cell_y)
+	if ((i + ni) >= 0 && (i + ni) < num_cell_x && (j + nj) >= 0 && (j + nj) < num_cell_y)
 	{
 		int w = terrain->at(i + ni).at(j + nj);
 		if (w != 0)
@@ -82,7 +90,7 @@ void Graph::CreateConnection(Node* node1, Node* node2, int weight = 1)
 	node1->adjacencyList.push_back(node2);
 	node2->adjacencyList.push_back(node1);
 	edgesMap.emplace(std::make_pair(node1, node2), new Edge(node1, node2, weight));
-	std::cout << "hola";
+	//std::cout << "hola";
 }
 
 //unused
